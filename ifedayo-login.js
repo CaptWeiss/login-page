02293ea580c0dc4fd6d4ac54ac1cd2f61abd1ac7
Ifedayojosh <adeleyeifedayojoshua@gmail.com>
2023-12-07 09:02:23 +0100

    javascript modified with else alert

--- ifedayo-login.js
+++ ifedayo-login.js
@@ -5,6 +5,9 @@ function validateform(){
     if (username == ""){  
         alert("Name can't be blank");  
          
+      }else
+      {
+      alert ("Congratulations New User");
       }
       if(password.length<6){  
         alert("Password must be at least 6 characters long.");    
